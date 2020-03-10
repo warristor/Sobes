44e4d3e1052ac1b6f984d64f4a4ff94b9ee1011c
warristor <doldmine@yandex.ru>
2020-03-10 20:20:54 +0300

    newformat commit

--- Prob/src/Sobes/Main.java
+++ Prob/src/Sobes/Main.java
@@ -9,12 +9,12 @@ import java.util.stream.Stream;
 public class Main {
 
 	// метод возвращает тариф по заданному диапазону параметров
-   static Double chooseTarifPrice(List<MTS> a, double min, double max) {  
+ static Double chooseTarifPrice(List<MTS> a, double min, double max) {  
 
-		for (MTS m : a) {
-			if (max >= m.getPrice() && min <= m.getPrice())
-				return m.getPrice();
-		}
+		    for (MTS m : a) {
+		         	if (max >= m.getPrice() && min <= m.getPrice())
+		  	            	return m.getPrice();
+     	}
                 return 0.0;
 	}
 
@@ -25,8 +25,8 @@ public class Main {
 		
 		//Создал список тарифов компании
 		List<MTS> ar = new ArrayList<>();            
-		ar.add(new Start(5, "Start", 23));
-		ar.add(new Bezlimit(11, "Bezlimit", 17));
+		                    ar.add(new Start(5, "Start", 23));
+		                    ar.add(new Bezlimit(11, "Bezlimit", 17));
 		
 						
 
@@ -35,16 +35,16 @@ public class Main {
 		System.out.println(st.mapToInt(MTS::getClient).sum());
 		
 		
-		//создаем компаратор для сортировки по цене тарифа
-        st=ar.stream();
-        List<MTS>ll=st.sorted((o, o1) -> o.getPrice().compareTo(o1.getPrice()))
-        		      .collect(Collectors.toList());
+	//создаем компаратор для сортировки по цене тарифа
+ st=ar.stream();
+ List<MTS>ll=st.sorted((o, o1) -> o.getPrice().compareTo(o1.getPrice()))
+        		                 .collect(Collectors.toList());
         
-        System.out.println(ll);
+System.out.println(ll);
         	
         
-		// выбирает тариф по заданному диапазону
-        System.out.println(chooseTarifPrice(ll, 20, 30));
+// выбирает тариф по заданному диапазону
+System.out.println(chooseTarifPrice(ll, 20, 30));
 
 	}
 
