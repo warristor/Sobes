6c69813c0c1db6a4737daf3a6853f58adae53c54
warristor <doldmine@yandex.ru>
2020-03-10 20:25:34 +0300

    form_commit

--- Prob/src/Sobes/Main.java
+++ Prob/src/Sobes/Main.java
@@ -9,43 +9,40 @@ import java.util.stream.Stream;
 public class Main {
 
 	// метод возвращает тариф по заданному диапазону параметров
- static Double chooseTarifPrice(List<MTS> a, double min, double max) {  
+    static Double chooseTarifPrice(List<MTS> a, double min, double max) {  
 
 		    for (MTS m : a) {
-		         	if (max >= m.getPrice() && min <= m.getPrice())
-		  	            	return m.getPrice();
-     	}
-                return 0.0;
+		       	if (max >= m.getPrice() && min <= m.getPrice())
+		  	       	return m.getPrice();
+     	    }
+        return 0.0;
 	}
-
    
 
-	public static void main(String[] args) {
+ public static void main(String[] args) {
 		
 		
-		//Создал список тарифов компании
-		List<MTS> ar = new ArrayList<>();            
-		                    ar.add(new Start(5, "Start", 23));
-		                    ar.add(new Bezlimit(11, "Bezlimit", 17));
-		
-						
+   //Создал список тарифов компании
+   List<MTS> ar = new ArrayList<>();            
+   ar.add(new Start(5, "Start", 23));
+   ar.add(new Bezlimit(11, "Bezlimit", 17));
+							
 
-		// подсчитали общее количество клиентов
-		Stream<MTS> st=ar.stream();
-		System.out.println(st.mapToInt(MTS::getClient).sum());
+   // подсчитали общее количество клиентов
+   Stream<MTS> st=ar.stream();
+   System.out.println(st.mapToInt(MTS::getClient).sum());
 		
 		
-	//создаем компаратор для сортировки по цене тарифа
- st=ar.stream();
- List<MTS>ll=st.sorted((o, o1) -> o.getPrice().compareTo(o1.getPrice()))
-        		                 .collect(Collectors.toList());
+   //создаем компаратор для сортировки по цене тарифа
+   st=ar.stream();
+   List<MTS>ll=st.sorted((o, o1) -> o.getPrice().compareTo(o1.getPrice())).collect(Collectors.toList());
         
-System.out.println(ll);
+   System.out.println(ll);
         	
         
-// выбирает тариф по заданному диапазону
-System.out.println(chooseTarifPrice(ll, 20, 30));
+   // выбирает тариф по заданному диапазону
+   System.out.println(chooseTarifPrice(ll, 20, 30));
 
-	}
+ }
 
 }
