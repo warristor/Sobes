5cde0ad237b200107721c1e86bb2487f256a333c
warristor <doldmine@yandex.ru>
2020-03-10 20:00:48 +0300

    next commit

--- Prob/src/Sobes/Main.java
+++ Prob/src/Sobes/Main.java
@@ -3,49 +3,46 @@ package Sobes;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
 	// метод возвращает тариф по заданному диапазону параметров
-    static void chooseTarifPrice(List<MTS> a, double min, double max) {  
+   static Double chooseTarifPrice(List<MTS> a, double min, double max) {  
 
 		for (MTS m : a) {
-			if (max >= m.getPrice() | min <= m.getPrice())
-				System.out.println(m);
+			if (max >= m.getPrice() && min <= m.getPrice())
+				return m.getPrice();
 		}
-
+                return 0.0;
 	}
 
-	//подсчитывает количество клиентов////
-	static int getSumClient(List<MTS> a) { 
-
-		int sum = 0;
-		for (MTS m : a) {
-			sum += m.getClient();
-		}
-		return sum;
-	}
+   
 
 	public static void main(String[] args) {
 		
 		
 		//Создал список тарифов компании
 		List<MTS> ar = new ArrayList<>();            
-		ar.add(new Bezlimit(11, "Bezlimit", 17));
 		ar.add(new Start(5, "Start", 23));
-
+		ar.add(new Bezlimit(11, "Bezlimit", 17));
 		
-		// подсчитали общее количество клиентов
-		System.out.println(getSumClient(ar));       
+						
 
+		// подсчитали общее количество клиентов
+		Stream<MTS> st=ar.stream();
+		System.out.println(st.mapToInt(MTS::getClient).sum());
+		
 		
 		//создаем компаратор для сортировки по цене тарифа
-		Comparator<MTS> comp = (o, o1) -> o.getPrice().compareTo(o1.getPrice());
-		ar.sort(comp);
-		System.out.println(ar);
-
+        st=ar.stream();
+        List<MTS>ll=st.sorted((o, o1) -> o.getPrice().compareTo(o1.getPrice())).collect(Collectors.toList());
+        System.out.println(ll);
+        	
+        
 		// выбирает тариф по заданному диапазону
-		chooseTarifPrice(ar, 20, 30);
+        System.out.println(chooseTarifPrice(ll, 20, 30));
 
 	}
 
