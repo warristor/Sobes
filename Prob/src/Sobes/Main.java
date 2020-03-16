bc48eff4cdda9e52a9c4fc384a18cb5ed42c3514
warristor <doldmine@yandex.ru>
2020-03-16 13:37:35 +0300

    corect_commit

--- Prob/src/Sobes/Main.java
+++ Prob/src/Sobes/Main.java
@@ -3,44 +3,44 @@ package Sobes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Main {
 
-	//метод возвращает тариф по заданному диапазону параметров
-	static Optional<MTS> chooseTarifPrice(List<MTS> listMTS, double min, double max) {
+    // метод возвращает тариф по заданному диапазону параметров
+    static Optional<MTS> chooseTarifPrice(List<MTS> listMTS, double min, double max) {
 
-		return listMTS.stream().filter(obj -> max >= obj.getPrice() && min >= obj.getPrice())
-			               .findAny();
-	}
+	return listMTS.stream().filter(obj -> max >= obj.getPrice() && min >= obj.getPrice())
+		               .findAny();
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Создал список тарифов компании
-		List<MTS> listMTS = new ArrayList<>();
-		listMTS.add(new Start(5, "Start", 23));
-		listMTS.add(new Bezlimit(11, "Bezlimit", 17));
+	// Создал список тарифов компании
+	List<MTS> listMTS = new ArrayList<>();
+	listMTS.add(new Start(5, "Start", 23));
+	listMTS.add(new Bezlimit(11, "Bezlimit", 17));
 
-		// подсчитали общее количество клиентов
-		Optional<Integer> sum = listMTS.stream().map(MTS::getClient)
-				                        .reduce((intClient1, intClient2) -> intClient1 + intClient2);
+	// подсчитали общее количество клиентов
+	Integer sum = listMTS.stream().mapToInt(MTS::getClient).sum();
 
-		// выводим на консоль сумму клиентов
-		System.out.println("Общее количество клиентов: " + sum);
+	// выводим на консоль сумму клиентов
+	System.out.println("Общее количество клиентов: " + sum);
 
-		// System.out.println("Общее количество клиентов: "+listMTS.stream()
-		// .mapToInt(MTS::getClient).sum());
+//		Optional<Integer> sum = listMTS.stream().map(MTS::getClient)
+//                .reduce((intClient1, intClient2) -> intClient1 + intClient2);
 
-		// сортируем тарифы по цене
-		List<MTS> listSorted = listMTS.stream().sorted((obj, obj1) -> obj.getPrice().compareTo(obj1.getPrice()))
-				                       .collect(Collectors.toList());
+	// сортируем тарифы по цене
+	listMTS.sort((obj, obj1) -> obj.getPrice().compareTo(obj1.getPrice()));
 
-		// выводим на консоль отсортированый список
-		System.out.println(listSorted);
+//		List<MTS> listSorted = listMTS.stream().sorted((obj, obj1) -> obj.getPrice().compareTo(obj1.getPrice()))
+//				                       .collect(Collectors.toList());
 
-		// выводим тариф на консоль по заданному диапазону
-		System.out.println(chooseTarifPrice(listSorted, 17, 22));
+	// выводим на консоль отсортированый список
+	System.out.println(listMTS);
 
-	}
+	// выводим тариф на консоль по заданному диапазону
+	System.out.println(chooseTarifPrice(listMTS, 17, 22));
+
+    }
 
 }
