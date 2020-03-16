2ee443860158fcc463ae305b2e66cc39dd79669c
warristor <doldmine@yandex.ru>
2020-03-16 13:19:48 +0300

    new_commit

--- Prob/src/Sobes/Main.java
+++ Prob/src/Sobes/Main.java
@@ -1,58 +1,46 @@
 package Sobes;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
 public class Main {
-		
-	
-	// метод возвращает тариф по заданному диапазону параметров
-    
-	
-	
-	static Optional<MTS> chooseTarifPrice(List<MTS> a, double min, double max) throws Exception {  
 
-    	    	
-		    for (MTS m : a) {
-		       	if (max >= m.getPrice() && min >= m.getPrice())
-		  	       	return Optional.of(m);
-     	    }
-        return Optional.empty();
-        	    //Collections.min(a,((b,c)->b.getPrice().compareTo(c.getPrice())));
-        		//a.stream().min((b,c)->b.getPrice().compareTo(c.getPrice())).get();
-     }
-	
-			
-	    
- public static void main(String[] args) throws Exception  {
-		 
-	 
-   //Создал список тарифов компании
-   List<MTS> ar = new ArrayList<>();            
-   ar.add(new Start(5, "Start", 23));
-   ar.add(new Bezlimit(11, "Bezlimit", 17));
-			  
+	//метод возвращает тариф по заданному диапазону параметров
+	static Optional<MTS> chooseTarifPrice(List<MTS> listMTS, double min, double max) {
 
-   // подсчитали общее количество клиентов
-   System.out.println("Общее количество клиентов: "+ar.stream()
-                                                      .mapToInt(MTS::getClient).sum());
-		
-		
-   //создаем компаратор для сортировки по цене тарифа
-   List<MTS>ll=ar.stream().sorted((o, o1) -> o.getPrice().compareTo(o1.getPrice()))
-		                  .collect(Collectors.toList());
-        
-   System.out.println(ll);
-       	
-        
-   // выбирает тариф по заданному диапазону
-   
-     System.out.println(chooseTarifPrice(ll, 16, 22));
+		return listMTS.stream().filter(obj -> max >= obj.getPrice() && min >= obj.getPrice())
+			.findAny();
+	}
 
- }
+	public static void main(String[] args) {
+
+		// Создал список тарифов компании
+		List<MTS> listMTS = new ArrayList<>();
+		listMTS.add(new Start(5, "Start", 23));
+		listMTS.add(new Bezlimit(11, "Bezlimit", 17));
+
+		// подсчитали общее количество клиентов
+		Optional<Integer> sum = listMTS.stream().map(MTS::getClient)
+				.reduce((intClient1, intClient2) -> intClient1 + intClient2);
+
+		// выводим на консоль сумму клиентов
+		System.out.println("Общее количество клиентов: " + sum);
+
+		// System.out.println("Общее количество клиентов: "+listMTS.stream()
+		// .mapToInt(MTS::getClient).sum());
+
+		// сортируем тарифы по цене
+		List<MTS> listSorted = listMTS.stream().sorted((obj, obj1) -> obj.getPrice().compareTo(obj1.getPrice()))
+				.collect(Collectors.toList());
+
+		// выводим на консоль отсортированый список
+		System.out.println(listSorted);
+
+		// выводим тариф на консоль по заданному диапазону
+		System.out.println(chooseTarifPrice(listSorted, 17, 22));
+
+	}
 
 }
